5899aac04179747514a8e35d48c87c2f6f040489
gracetheo <b07902085@ntu.edu.tw>
2023-02-03 00:31:28 +0700

    modify gsap

--- frontend/src/pages/gsap/index.js
+++ frontend/src/pages/gsap/index.js
@@ -1,89 +1,133 @@
 import React, { useRef, useState, useEffect } from "react";
 import { ReactComponent as Logo } from "../../images/logo.svg";
 import { ReactComponent as Hand } from "../../images/hand.svg";
-import { ReactComponent as FrontGrass } from "../../images/frontGrass.svg";
-import { ReactComponent as BehindGrass } from "../../images/behindGrass.svg";
-import { ReactComponent as Cloud } from "../../images/cloud.svg";
-import { ReactComponent as Mountain } from "../../images/mountain.svg";
+import { ReactComponent as FrontCenter } from "../../images/frontCenter.svg";
+import { ReactComponent as FrontLeft } from "../../images/frontLeft.svg";
+import { ReactComponent as FrontRight } from "../../images/frontRight.svg";
+import { ReactComponent as BackCenter } from "../../images/backCenter.svg";
+import { ReactComponent as BackRight } from "../../images/backRight.svg";
+import { ReactComponent as CloudLeft } from "../../images/cloudLeft.svg";
+import { ReactComponent as CloudRight } from "../../images/cloudRight.svg";
+import { ReactComponent as MountainLeft } from "../../images/mountainLeft.svg";
+import { ReactComponent as MountainRight } from "../../images/mountainRight.svg";
 import styles from "./styles.module.scss";
 
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-// gsap.registerPlugin(ScrollTrigger);
-// import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.config({ ignoreMobileResize: true });
+// ScrollTrigger.normalizeScroll(true);
 
 export const Gsap = ({}) => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [windowHeight, setWindowHeight] = useState(window.innerHeight);
   const handRef = useRef(null);
+  const [second, setSecond] = useState(false);
 
   useEffect(() => {
     console.log("done rendering");
-    const handTl = gsap.timeline({
-      scrollTrigger: {
-        trigger: "#hand",
-        markers: true,
-        start: "top 70%",
-        end: "top 25%",
-        scrub: 1
-      }
-    });
 
-    handTl.to("#hand", {yPercent: -60, duration: 5});
+    let handScrollTrigger = {scrollTrigger: {
+      trigger: "#trigger",
+      // markers: true,
+      start: "bottom 150%",
+      end: "bottom 0%",
+      scrub: 0
+    }}
 
-    const grassTl = gsap.timeline({
-      scrollTrigger: {
-        trigger: "#hand",
-        markers: true,
-        start: "top 70%",
-        end: "top 25%",
-        scrub: 1
-      }
-    });
+    const handTl = gsap.timeline(handScrollTrigger);
+    handTl.to("#hand", {y: "-65%", duration: 10, ease: "sine.inOut"});
+    handTl.to("#hand", {y: "-85%", duration: 5, ease: "sine.inOut"});
 
-    grassTl.to(["#frontGrass", "#backGrass"], {yPercent: 60, xPercent: -3, width: "110%", duration: 5});
+    const grassFrontCenterTl = gsap.timeline(handScrollTrigger);
+    grassFrontCenterTl.to("#frontCenter", {y: "40%", x: "-50%", width: "120%", duration: 10, ease: "sine.inOut"});
+    grassFrontCenterTl.to("#frontCenter", {y: "-20%", duration: 5, ease: "sine.inOut"});
 
-    const logoTl = gsap.timeline({
-      scrollTrigger: {
-        trigger: "#logo",
-        markers: true,
-        start: "bottom 15%",
-        end: "bottom 0%",
-        scrub: 1
-      }
-    });
+    const grassBackCenterTl = gsap.timeline(handScrollTrigger);
+    grassBackCenterTl.to("#backCenter", {y: "30%", x: "-50%", width: "120%", duration: 10, ease: "sine.inOut"});
+    grassBackCenterTl.to("#backCenter", {y: "-5%", duration: 5, ease: "sine.inOut"});
 
-    logoTl.to("#logo", {yPercent: -80, xPercent: -3, width: "80vw", duration: 1});
+    const grassFrontLeftTl = gsap.timeline(handScrollTrigger);
+    grassFrontLeftTl.to("#frontLeft", {y: "35%", x: "-20%", duration: 10, ease: "sine.inOut"});
+    grassFrontLeftTl.to("#frontLeft", {y: "-20%", x: "-30%", duration: 5, ease: "sine.inOut"});
 
-    // const onResize = () => {
-    //   var hand = document.querySelector('#hand');
-    //   hand.style.left = "50%";
-    //   hand.style.transform = "translateX(-50%)";
-      
-    //   var logo = document.querySelector('#logo');
-    //   logo.style.left = "50%";
-    //   logo.style.transform = "translateX(-50%)";
-    // }
+    const grassFrontRightTl = gsap.timeline(handScrollTrigger);
+    grassFrontRightTl.to("#frontRight", {y: "35%", x: "30%", duration: 10, ease: "sine.inOut"});
+    grassFrontRightTl.to("#frontRight", {y: "-20%", x: "40%", duration: 5, ease: "sine.inOut"});
 
-    // window.addEventListener("resize", onResize);
+    const grassBackRightTl = gsap.timeline(handScrollTrigger);
+    grassBackRightTl.to("#backRight", {y: "35%", x: "10%", duration: 10, ease: "sine.inOut"});
+    grassBackRightTl.to("#backRight", {y: "-20%", x: "20%", duration: 5, ease: "sine.inOut"});
 
-    // return(() => {
-    //   window.removeEventListener("resize", onResize);
-    // });
+    const mountainLeftTl = gsap.timeline(handScrollTrigger);
+    mountainLeftTl.to("#mountainLeft", {y: "20%", x: "-10%", duration: 10, ease: "sine.inOut"});
+    mountainLeftTl.to("#mountainLeft", {y: "-5%", duration: 5, ease: "sine.inOut"});
 
+    const mountainRightTl = gsap.timeline(handScrollTrigger);
+    mountainRightTl.to("#mountainRight", {y: "20%", x: "10%", duration: 10, ease: "sine.inOut"});
+    mountainRightTl.to("#mountainRight", {y: "-5%", duration: 5, ease: "sine.inOut"});
+
+    const cloudLeftTl = gsap.timeline(handScrollTrigger);
+    cloudLeftTl.to("#cloudLeft", {y: "10%", x: "-5%", duration: 10, ease: "sine.inOut"});
+    cloudLeftTl.to("#cloudLeft", {y: "0%", duration: 5, ease: "sine.inOut"});
+
+    const cloudRightTl = gsap.timeline(handScrollTrigger);
+    cloudRightTl.to("#cloudRight", {y: "10%", x: "5%", duration: 10, ease: "sine.inOut"});
+    cloudRightTl.to("#cloudRight", {y: "0%", duration: 5, ease: "sine.inOut"});
+
+    const logoTl = gsap.timeline(handScrollTrigger);
+    logoTl.to("#logo", {y: "-70%", x: "-50%", width: "80%", duration: 10, ease: "sine.inOut"});
+    logoTl.to("#logo", {opacity: 0, duration: 5});
+
+    window.addEventListener("scroll", handleOnScroll);
+
+    return(() => {
+      window.removeEventListener("scroll", handleOnScroll);
+    })
   }, []);
 
+  const handleOnScroll = () => {
+    // let percentage = window.pageYOffset / window.innerHeight;
+    // console.log(percentage);
+    // if (percentage >= 1.5) {
+    //   setSecond(true);
+    // } else {
+    //   setSecond(false);
+    // }
+
+    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 2) {
+      setSecond(true);
+    } else {
+      setSecond(false);
+    }
+  }
+
   return (
-    <div className={styles.container}>
-      {/* <div style={{ height: "80vh", backgroundColor: "pink" }} /> */}
-      {/* <div className={styles.panel} /> */}
-      <Cloud id="cloud" className={styles.cloud} />
-      <Mountain id="mountain" className={styles.mountain} />
-      <Logo id="logo" className={styles.logo} />
-      <FrontGrass id="frontGrass" className={styles.frontGrass} />
-      <Hand id="hand" className={styles.hand} />
-      <BehindGrass id="backGrass" className={styles.backGrass} />
+    <div id="container" className={styles.container}>
+      <div id="trigger" className={styles.trigger} />
+
+      <div className={`${styles.background} ${second ? styles.target : ""}`}>
+        <CloudLeft id="cloudLeft" className={styles.cloud} />
+        <CloudRight id="cloudRight" className={styles.cloud} />
+        <MountainLeft id="mountainLeft" className={`${styles.mountain} ${styles.left}`} />
+        <MountainRight id="mountainRight" className={`${styles.mountain} ${styles.right}`} />
+        <Logo id="logo" className={styles.logo} />
+        <BackRight id="backRight" className={`${styles.back} ${styles.right}`} />
+        <BackCenter id="backCenter" className={`${styles.back} ${styles.center}`} />
+        <Hand id="hand" className={styles.hand} />
+        <FrontLeft id="frontLeft" className={`${styles.front} ${styles.left}`} />
+        <FrontRight id="frontRight" className={`${styles.front} ${styles.right}`} />
+        <FrontCenter id="frontCenter" className={`${styles.front} ${styles.center}`} />
+      </div>
+
+      <div className={styles.cutOutContainer}>
+        <div className={`${styles.circleCutOut} ${second ? styles.target : ""}`}>
+          <div className={`${styles.circleLeft} ${second ? styles.target : ""}`} />
+          <div className={`${styles.circleCenter} ${second ? styles.target : ""}`} />
+          <div className={`${styles.circleRight} ${second ? styles.target : ""}`} />
+        </div>
+        <div className={`${styles.squareCutOut} ${second ? styles.target : ""}`} />
+      </div>
     </div>
   )
 }
