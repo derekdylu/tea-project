48df30de315e2fa88aa08afd3b533c0e63dde072
gracetheo <b07902085@ntu.edu.tw>
2023-02-03 21:45:53 +0700

    Compressed svg (png to vector)

--- frontend/src/pages/gsap/index.js
+++ frontend/src/pages/gsap/index.js
@@ -1,15 +1,15 @@
 import React, { useRef, useState, useEffect } from "react";
 import { ReactComponent as Logo } from "../../images/logo.svg";
-import { ReactComponent as Hand } from "../../images/hand.svg";
-import { ReactComponent as FrontCenter } from "../../images/frontCenter.svg";
-import { ReactComponent as FrontLeft } from "../../images/frontLeft.svg";
-import { ReactComponent as FrontRight } from "../../images/frontRight.svg";
-import { ReactComponent as BackCenter } from "../../images/backCenter.svg";
-import { ReactComponent as BackRight } from "../../images/backRight.svg";
-import { ReactComponent as CloudLeft } from "../../images/cloudLeft.svg";
-import { ReactComponent as CloudRight } from "../../images/cloudRight.svg";
-import { ReactComponent as MountainLeft } from "../../images/mountainLeft.svg";
-import { ReactComponent as MountainRight } from "../../images/mountainRight.svg";
+import { ReactComponent as Hand } from "../../images/home/hand.svg";
+import { ReactComponent as FrontCenter } from "../../images/home/frontCenter.svg";
+import { ReactComponent as FrontLeft } from "../../images/home/frontLeft.svg";
+import { ReactComponent as FrontRight } from "../../images/home/frontRight.svg";
+import { ReactComponent as BackCenter } from "../../images/home/backCenter.svg";
+import { ReactComponent as BackRight } from "../../images/home/backRight.svg";
+import { ReactComponent as CloudLeft } from "../../images/home/cloudLeft.svg";
+import { ReactComponent as CloudRight } from "../../images/home/cloudRight.svg";
+import { ReactComponent as MountainLeft } from "../../images/home/mountainLeft.svg";
+import { ReactComponent as MountainRight } from "../../images/home/mountainRight.svg";
 import styles from "./styles.module.scss";
 
 import gsap from "gsap";
@@ -40,44 +40,44 @@ export const Gsap = ({}) => {
     handTl.to("#hand", {y: "-85%", duration: 5, ease: "sine.inOut"});
 
     const grassFrontCenterTl = gsap.timeline(handScrollTrigger);
-    grassFrontCenterTl.to("#frontCenter", {y: "40%", x: "-50%", width: "120%", duration: 10, ease: "sine.inOut"});
-    grassFrontCenterTl.to("#frontCenter", {y: "-20%", duration: 5, ease: "sine.inOut"});
+    grassFrontCenterTl.to("#frontCenter", {y: "30%", x: "-50%", width: "120%", duration: 10, ease: "sine.inOut"});
+    grassFrontCenterTl.to("#frontCenter", {y: "-12%", duration: 5, ease: "sine.inOut"});
 
     const grassBackCenterTl = gsap.timeline(handScrollTrigger);
-    grassBackCenterTl.to("#backCenter", {y: "30%", x: "-50%", width: "120%", duration: 10, ease: "sine.inOut"});
-    grassBackCenterTl.to("#backCenter", {y: "-5%", duration: 5, ease: "sine.inOut"});
+    grassBackCenterTl.to("#backCenter", {y: "40%", x: "-50%", width: "120%", duration: 10, ease: "sine.inOut"});
+    grassBackCenterTl.to("#backCenter", {y: "-30%", duration: 5, ease: "sine.inOut"});
 
     const grassFrontLeftTl = gsap.timeline(handScrollTrigger);
-    grassFrontLeftTl.to("#frontLeft", {y: "35%", x: "-20%", duration: 10, ease: "sine.inOut"});
-    grassFrontLeftTl.to("#frontLeft", {y: "-20%", x: "-30%", duration: 5, ease: "sine.inOut"});
+    grassFrontLeftTl.to("#frontLeft", {y: "50%", x: "-8%", duration: 10, ease: "sine.inOut"});
+    grassFrontLeftTl.to("#frontLeft", {y: "-20%", x: "-25%", duration: 5, ease: "sine.inOut"});
 
     const grassFrontRightTl = gsap.timeline(handScrollTrigger);
-    grassFrontRightTl.to("#frontRight", {y: "35%", x: "30%", duration: 10, ease: "sine.inOut"});
-    grassFrontRightTl.to("#frontRight", {y: "-20%", x: "40%", duration: 5, ease: "sine.inOut"});
+    grassFrontRightTl.to("#frontRight", {y: "50%", x: "30%", duration: 10, ease: "sine.inOut"});
+    grassFrontRightTl.to("#frontRight", {y: "-5%", x: "35%", duration: 5, ease: "sine.inOut"});
 
     const grassBackRightTl = gsap.timeline(handScrollTrigger);
-    grassBackRightTl.to("#backRight", {y: "35%", x: "10%", duration: 10, ease: "sine.inOut"});
-    grassBackRightTl.to("#backRight", {y: "-20%", x: "20%", duration: 5, ease: "sine.inOut"});
+    grassBackRightTl.to("#backRight", {y: "12%", x: "10%", duration: 10, ease: "sine.inOut"});
+    grassBackRightTl.to("#backRight", {y: "-50%", x: "15%", duration: 5, ease: "sine.inOut"});
 
     const mountainLeftTl = gsap.timeline(handScrollTrigger);
-    mountainLeftTl.to("#mountainLeft", {y: "20%", x: "-10%", duration: 10, ease: "sine.inOut"});
-    mountainLeftTl.to("#mountainLeft", {y: "-5%", duration: 5, ease: "sine.inOut"});
+    mountainLeftTl.to("#mountainLeft", {y: "15%", x: "-25%", duration: 10, ease: "sine.inOut"});
+    mountainLeftTl.to("#mountainLeft", {y: "-30%", duration: 5, ease: "sine.inOut"});
 
     const mountainRightTl = gsap.timeline(handScrollTrigger);
-    mountainRightTl.to("#mountainRight", {y: "20%", x: "10%", duration: 10, ease: "sine.inOut"});
-    mountainRightTl.to("#mountainRight", {y: "-5%", duration: 5, ease: "sine.inOut"});
+    mountainRightTl.to("#mountainRight", {y: "15%", x: "20%", duration: 10, ease: "sine.inOut"});
+    mountainRightTl.to("#mountainRight", {y: "-30%", duration: 5, ease: "sine.inOut"});
 
     const cloudLeftTl = gsap.timeline(handScrollTrigger);
-    cloudLeftTl.to("#cloudLeft", {y: "10%", x: "-5%", duration: 10, ease: "sine.inOut"});
-    cloudLeftTl.to("#cloudLeft", {y: "0%", duration: 5, ease: "sine.inOut"});
+    cloudLeftTl.to("#cloudLeft", {y: "12%", x: "-5%", duration: 10, ease: "sine.inOut"});
+    cloudLeftTl.to("#cloudLeft", {y: "-30%", duration: 5, ease: "sine.inOut"});
 
     const cloudRightTl = gsap.timeline(handScrollTrigger);
-    cloudRightTl.to("#cloudRight", {y: "10%", x: "5%", duration: 10, ease: "sine.inOut"});
-    cloudRightTl.to("#cloudRight", {y: "0%", duration: 5, ease: "sine.inOut"});
+    cloudRightTl.to("#cloudRight", {y: "12%", x: "5%", duration: 10, ease: "sine.inOut"});
+    cloudRightTl.to("#cloudRight", {y: "-30%", duration: 5, ease: "sine.inOut"});
 
     const logoTl = gsap.timeline(handScrollTrigger);
     logoTl.to("#logo", {y: "-70%", x: "-50%", width: "80%", duration: 10, ease: "sine.inOut"});
-    logoTl.to("#logo", {opacity: 0, duration: 5});
+    logoTl.to("#logo", {y: "-80%", opacity: 0, duration: 5});
 
     window.addEventListener("scroll", handleOnScroll);
 
