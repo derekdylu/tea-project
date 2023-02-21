1b5276224a6ffa5ccd714d25c1722b636b400149
Derek Lu <derekdylu@gmail.com>
2023-02-21 22:02:02 +0800

    merge with grc correctly

--- frontend/src/Containers/Live/index.js
+++ frontend/src/Containers/Live/index.js
@@ -17,12 +17,20 @@ export const Live = ({
     "tea10": 90,
   });
 
+  const position = ["2%", "12%", "22%", "32%", "42%", "52%", "62%", "72%", "82%", "92%"];
+
   const [max, setMax] = useState(0);
   const [sortedKeys, setSortedKeys] = useState([]);
 
   useEffect(() => {
     setMax(Math.max.apply(null, Object.values(rank)));
+    let sorted = Object.keys(rank).sort(function(a, b) {return rank[b] - rank[a]});
     setSortedKeys(Object.keys(rank).sort(function(a, b) {return rank[b] - rank[a]}));
+    sorted.map((key, i) => {
+      let element = document.getElementById(key)
+      console.log(element.style)
+      element.style.top = position[i]
+    })
   }, [rank]);
 
   function handleOnClick(key) {
@@ -34,8 +42,8 @@ export const Live = ({
   return (
     <div className={styles.container}>
       {/* <div className={styles.graph}> */}
-        { sortedKeys.map((key) => (
-          <div className={styles.row}>
+        { Object.keys(rank).map((key) => (
+          <div className={styles.row} id={key}>
             <div onClick={ (e) => handleOnClick(key) }>{key} {rank[key]}</div>
             <div className={styles.bar} style={{width: `${rank[key] / max * 100}%`}} />
           </div>
