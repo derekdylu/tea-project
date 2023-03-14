ceca6be7c8b43bad332587b3e4c9e9e2dcd59bfe
gracetheo <b07902085@ntu.edu.tw>
2023-03-15 01:25:51 +0800

    - Finalize term json - Add animation on share page - Add create png function

--- frontend/src/Components/Share/index.js
+++ frontend/src/Components/Share/index.js
@@ -1,20 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ReactComponent as Flower } from "../../Images/flower.svg";
 import styles from "./styles.module.scss";
-import { Typography } from "@mui/material";
 import taste1 from "../../Images/Share/taste1.png"
 import taste2 from "../../Images/Share/taste2.png"
 import { ReactComponent as DownloadIcon } from "../../Images/Icon/download.svg"
 
-export const Share = ({ teaName, teaSubName, hashtags }) => {
-  // const teaName = ["四季春"];
-  // const teaSubName = "茶樹品種"
-  // const hashtags = ["清香", "淡雅",	"臺灣綠茶", "不發酵茶"]
+import { ThemeProvider } from "@emotion/react";
+import theme from "../../Themes/Theme";
+import { Typography } from "@mui/material";
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import { toPng as htmlToImage } from 'html-to-image';
+import Marquee from "react-fast-marquee";
+
+export const Share = ({
+  teaName=[""],
+  teaSubName="",
+  hashtags=["",""],
+}) => {
+  const [showShareDialog, setShowShareDialog] = useState(false);
+  const [showButton, setShowButton] = useState(true);
+  const [type, setType] = useState(true);
+
+  const onShare = () => {
+    setShowShareDialog(true);
+    setShowButton(false);
+
+    var node = document.getElementById('capture');
+    setTimeout(() => {
+      htmlToImage(node)
+        .then(function (dataUrl) {
+          let render = document.getElementById('renderer')
+          render.innerHTML = "<img src='" + dataUrl + "' alt='render' width='100%' />"
+        })
+        .catch(function (error) {
+          console.error('oops, something went wrong!', error);
+        });
+    }, 500)
+  }
+
+  const handleCloseDialog = () => {
+    setShowButton(true);
+    setShowShareDialog(false);
+  }
 
   return (
-    // <div style={{width: "100vw", height: "100vh"}}>
+    <ThemeProvider theme={theme}>
       <div className={styles.container}>
-        <div className={styles.shareContainer}>
+        <div className={styles.shareContainer} id="capture">
           <Flower className={styles.flower} />
           <div className={styles.text}>
             <div className={styles.row}>
@@ -49,19 +82,38 @@ export const Share = ({ teaName, teaSubName, hashtags }) => {
               </div>
             </div>
           </div>
-          <div className={styles.imageContainer}>
-            <img src={taste1} />
-            <img src={taste2} />
-            <img src={taste1} />
-            <img src={taste2} />
+          <Typography variant="displayMedium">
+            不是只有喝的。不是只有喝的。
+          </Typography>
+          
+          <div className={styles.wrapper}>
+            <div className={styles.marquee}>
+              <div>
+                <img src={taste1}/>
+                <img src={taste2} />
+                <img src={taste1} />
+                <img src={taste2} />
+              </div>
+              <div>
+                <img src={taste1}/>
+                <img src={taste2} />
+                <img src={taste1} />
+                <img src={taste2} />
+              </div>
+            </div>
           </div>
         </div>
-        <button className={styles.button}>
+        {/* Download Button */}
+        { showButton &&
+          <button className={styles.createButton} onClick={() => onShare()}>
             <DownloadIcon />
             <Typography variant="labelLarge">
               下載結果
             </Typography>
           </button>
+        }
+
+        {/* Circle */}
         <svg className={styles.circleText}>
           <path
             id="curve"
@@ -78,7 +130,31 @@ export const Share = ({ teaName, teaSubName, hashtags }) => {
             </textPath>
           </text>
         </svg>
+
+        {/* Dialog */}
+        <Dialog
+          open={showShareDialog}
+          onClose={() => handleCloseDialog()}
+          PaperProps={{
+            style: { backgroundColor: theme.palette.surface.main, borderRadius: 28 }
+          }}
+        >
+          <DialogContent>
+            <div className={styles.dialog}>
+              <Typography variant="bodyMedium">
+                生成圖片後請長按下載圖片
+              </Typography>
+              <div id="renderer" />
+              
+              <button className={styles.button} onClick={handleCloseDialog}>
+                <Typography variant="labelLarge" color={theme.palette.primary.main}>
+                  關閉
+                </Typography>
+              </button>
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
-    // </div>
+    </ThemeProvider>
   )
 }
