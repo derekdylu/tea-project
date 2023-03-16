d7f4c291e5b7045ae21ab23cf749dc29e7f09605
Derek Lu <derekdylu@gmail.com>
2023-03-16 14:44:42 +0800

    add shown property in fend and bend

--- frontend/src/Utils/Axios.js
+++ frontend/src/Utils/Axios.js
@@ -18,12 +18,13 @@ export const getGames = async () => {
 }
 
 // create a game
-export const createGame = async (_selection, _selected, _decision, _timestamp) => {
+export const createGame = async (_selection, _selected, _decision, _timestamp, _shown) => {
   const param = JSON.stringify({
       selection: _selection,
       selected: _selected,
       decision: _decision,
-      timestamp: _timestamp
+      timestamp: _timestamp,
+      shown: _shown,
   });
 
   return await instance.post('/create_game', param, jsonHeader).then((res) => {
@@ -34,6 +35,35 @@ export const createGame = async (_selection, _selected, _decision, _timestamp) =
 // calculate a game
 export const calculateGame = async (id, selection) => {
   return await instance.put(`/calculate_game/${id}`, selection, jsonHeader).then((res) => {
+    return res.data;
+  })
+}
+
+export const updateGame = async (id, _selection, _selected, _decision, _timestamp, _shown) => {
+  let param = {}
+  
+  if (_selection != null) {
+    param = {...param, "selection": _selection}
+  }
+  
+  if (_selected != null) {
+    param = {...param, "selected": _selected}
+  }
+  
+  if (_decision != null) {
+    param = {...param, "decision": _decision}
+  }
+  
+  if (_timestamp != null) {
+    param = {...param, "timestamp": _timestamp}
+  }
+  
+  if (_shown != null) {
+    param = {...param, "shown": _shown}
+  }
+
+  param = JSON.stringify(param);
+  return instance.put(`/update_game/${id}`, param, jsonHeader).then((res) => {
     return res.data;
   })
 }
